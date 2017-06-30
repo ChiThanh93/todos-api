9d831d35394fbcc8eaf20cc0560a6028d75ca9b9
ChiThanh93 <n.c.thanh0801@gmail.com>
2017-06-30 15:02:30 +0700

    jwt

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170609040630) do
+ActiveRecord::Schema.define(version: 20170630034959) do
 
   create_table "items", force: :cascade do |t|
     t.string   "name"
@@ -26,6 +26,13 @@ ActiveRecord::Schema.define(version: 20170609040630) do
     t.string   "created_by"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
